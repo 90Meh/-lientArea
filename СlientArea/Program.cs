4ada2c7f896e7c7e814bb1516483e1e26906d572
90Meh <78521327+90Meh@users.noreply.github.com>
2024-10-03 00:22:39 +0800

    Add project files.

---
+++ СlientArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using GeometryLibrary;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Double radius = 5;
+        double circleArea = GeometryCalculator.CalculateCircleArea(radius);
+        Console.WriteLine($"Площадь круга с радиусом {radius} равна {circleArea}");
+
+        double sideA = 3;
+        double sideB = 4;
+        double sideC = 5;
+        double triangleArea = GeometryCalculator.CalculateTriangleArea(sideA, sideB, sideC);
+        Console.WriteLine($"Площадь треугольника со сторонами {sideA}, {sideB}, {sideC} равна {triangleArea}");
+    } 
+}
